d63862c0dfc0d222df78ff172661322054523abf
Maya Outhred <outhred@cs.washington.edu>
2024-05-22 06:11:26 -0700

    Rotating field of view for enemy objects

---
+++ Artifact/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+    public GameObject loseText;
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float disToTarget = Vector3.Distance(transform.position, target.position);
+                if (!Physics.Raycast(transform.position, dirToTarget, disToTarget, obstacleMask))
+                {
+                    target.gameObject.SetActive(false);
+                    loseText.SetActive(true);
+                }
+            }
+        }
+    }
+    public Vector3 directionFromAngle(float angle, bool globalAngle)
+    {
+        if (!globalAngle)
+        {
+            angle += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2f);
+        loseText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
